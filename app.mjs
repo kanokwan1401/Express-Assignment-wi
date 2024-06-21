b134f75f146664da1dccd65f982b37528866b2ff
wiKanok <kanokwan1401.ks@gmail.com>
2024-06-21 14:38:24 +0700

    feat-add status 200

--- app.mjs
+++ app.mjs
@@ -3,13 +3,15 @@ import express from "express";
 const app = express();
 const post = 4000;
 
+const userProfiles = {
+  data: {
+    name: "john",
+    age: 20,
+  },
+};
+
 app.get("/profiles", (req, res) => {
-  return res.json({
-    data: {
-      name: "john",
-      age: 20,
-    },
-  });
+  return res.status(200).json({ data: userProfiles });
 });
 
 app.listen(post, () => {
